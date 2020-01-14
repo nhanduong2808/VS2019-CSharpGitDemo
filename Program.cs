917a6ce93dc1365e52b2e6bd9d6e5b3e23a4ca9a
Nhan Duong <duong160@umn.edu>
2020-01-14 12:15:19 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VS2019_CSharpGitDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
